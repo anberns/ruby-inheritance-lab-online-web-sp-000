f6cddc8bf5f22c4ddf586a9c2d2a8bd14bd7e268
Aaron Berns <anberns@gmail.com>
2019-01-12 18:51:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,16 @@ require_relative "./user.rb"
 class Student < User 
   
   def initialize
-    @
+    @knowledge = []
+  end 
+  
+  def learn(knowledge)
+    @knowledge << knowledge
+  end 
+  
+  def knowledge
+    @knowledge
+  end
 
 end
+
